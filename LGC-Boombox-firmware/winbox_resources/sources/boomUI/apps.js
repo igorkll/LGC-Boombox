c762000a3b49dc04b9fb9124bd502c8a033c7b98
igorkll <frerther@gmail.com>
2025-08-17 12:50:59 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps.js
@@ -1,11 +1,23 @@
-let dockContainer = document.getElementById('dock');
+let appslist = document.getElementById('appslist');
+let appscontainer = document.getElementById('appscontainer');
 
 function addApp(name) {
     let appIcon = document.createElement('img');
     appIcon.id = `${name}_icon`;
     appIcon.src = `apps/${name}/icon.png`;
     appIcon.classList.add('dock-item');
-    dockContainer.appendChild(appIcon);
+    appslist.appendChild(appIcon);
+
+    fetch(`apps/${name}/app.html`)
+        .then(response => response.text())
+        .then(html => {
+            let appContainer = document.createElement('div');
+            appContainer.id = `${name}_container`;
+            appContainer.classList.add('app');
+            appContainer.innerHTML += html;
+            appscontainer.appendChild(appContainer);
+        })
+        .catch(err => console.error(err));
 }
 
 addApp("settings")
