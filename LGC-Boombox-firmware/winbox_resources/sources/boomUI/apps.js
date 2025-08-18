954140d3cf29f20478a730445bb14be86d5938ce
igorkll <frerther@gmail.com>
2025-08-18 21:36:43 +0300

    tranks igor (ECS)

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps.js
@@ -46,3 +46,11 @@ function addApp(name) {
 
 addApp("settings")
 addApp("music")
+addApp("settings")
+addApp("music")
+addApp("settings")
+addApp("music")
+addApp("settings")
+addApp("music")
+addApp("settings")
+addApp("music")
