2b783d85dbfd88d9fb97a301741827fc482350bc
igorkll <frerther@gmail.com>
2025-08-18 21:20:01 +0300

    backup

---
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/dock/element.js
@@ -0,0 +1,61 @@
+function isTouchingElement(touch, element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        touch.clientX >= rect.left &&
+        touch.clientX <= rect.right &&
+        touch.clientY >= rect.top &&
+        touch.clientY <= rect.bottom
+    );
+}
+
+function changeElementState(element, touch) {
+    if (touch) {
+        if (!element.classList.contains('active')) {
+            element.classList.add('dock-item-hover');
+        }
+    } else {
+        element.classList.remove('dock-item-hover');
+    }
+}
+
+function checkTouchs(event) {
+    document.querySelectorAll('.dock-item').forEach(element => {
+        let touch = false;
+        for (let i = 0; i < event.touches.length; i++) {
+            if (isTouchingElement(event.touches[i], element)) {
+                touch = true;
+                break;
+            }
+        }
+
+        if (touch) {
+            if (!element.classList.contains('active')) {
+                element.classList.add('dock-item-hover');
+            }
+        } else {
+            element.classList.remove('dock-item-hover');
+        }
+    });
+}
+
+document.addEventListener('touchstart', (event) => {
+    checkTouchs(event);
+});
+
+document.addEventListener('touchmove', (event) => {
+    checkTouchs(event);
+});
+
+document.addEventListener('touchend', (event) => {
+    checkTouchs(event);
+});
+
+document.querySelectorAll('.dock-item').forEach(element => {
+    element.addEventListener('mouseenter', (event) => {
+        changeElementState(element, true);
+    });
+
+    element.addEventListener('mouseleave', (event) => {
+        changeElementState(element, false);
+    });
+});
