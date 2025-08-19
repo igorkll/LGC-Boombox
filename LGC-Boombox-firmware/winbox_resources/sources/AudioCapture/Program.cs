5e4813daaf5423dbaae1c98c07fc0c67174cf8dd
igorkll <frerther@gmail.com>
2025-08-20 01:35:51 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/AudioCapture/Program.cs
+++ LGC-Boombox-firmware/winbox_resources/sources/AudioCapture/Program.cs
@@ -1,10 +1,18 @@
 ﻿using NAudio.CoreAudioApi;
 using NAudio.Wave;
+using NWaves.Features;
+using NWaves.Signals;
+using NWaves.Transforms;
 using System;
 using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
 
 class Program
 {
+    static int fftSize = 1024;
+
     static void Main()
     {
         var enumerator = new MMDeviceEnumerator();
@@ -16,7 +24,17 @@ class Program
         {
             capture.DataAvailable += (s, a) =>
             {
-                Console.WriteLine($"Captured {a.BytesRecorded} bytes");
+                DiscreteSignal spectrum = getSpectrum(a.Buffer, a.BytesRecorded, capture.WaveFormat.SampleRate);
+
+                int sampleRate = capture.WaveFormat.SampleRate;
+                for (int i = 0; i < spectrum.Length; i++)
+                {
+                    double freq = i * (double)sampleRate / fftSize;
+                    double amplitude = spectrum[i];
+                    Console.WriteLine($"{freq:F0} Hz : {amplitude:F4}");
+                }
+
+                Console.WriteLine("-----");
             };
 
             capture.RecordingStopped += (s, a) =>
@@ -29,5 +47,23 @@ class Program
             Console.ReadLine();
             capture.StopRecording();
         }
+    }
+
+    static DiscreteSignal getSpectrum(byte[] inputBuffer, int inputBufferSize, int sampleRate)
+    {
+        float[] samples = new float[inputBufferSize / 2];
+        for (int i = 0; i < samples.Length; i++)
+        {
+            short sample16 = BitConverter.ToInt16(inputBuffer, i * 2);
+            samples[i] = sample16 / 32768f;
+        }
+
+        int len = Math.Min(fftSize, samples.Length);
+        float[] buffer = new float[fftSize];
+        Array.Copy(samples, buffer, len);
+
+        DiscreteSignal signal = new DiscreteSignal(sampleRate, buffer);
+        Fft fft = new Fft(fftSize);
+        return fft.PowerSpectrum(signal);
     }
 }
