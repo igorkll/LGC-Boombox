b7b5859403c14961a27514fa7fcc273d657e5775
igorkll <frerther@gmail.com>
2025-08-20 01:13:55 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/AudioCapture/Program.cs
+++ LGC-Boombox-firmware/winbox_resources/sources/AudioCapture/Program.cs
@@ -7,18 +7,15 @@ class Program
 {
     static void Main()
     {
-        // Получаем дефолтное устройство вывода
         var enumerator = new MMDeviceEnumerator();
         var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
 
         Console.WriteLine($"Using device: {device.FriendlyName}");
 
-        // Создаём loopback захват
         using (var capture = new WasapiLoopbackCapture(device))
         {
             capture.DataAvailable += (s, a) =>
             {
-                // a.Buffer содержит PCM данные
                 Console.WriteLine($"Captured {a.BytesRecorded} bytes");
             };
 
