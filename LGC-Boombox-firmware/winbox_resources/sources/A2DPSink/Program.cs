8a182f387c840ecbd40388043bbc7055b3201c32
igorkll <frerther@gmail.com>
2025-08-19 23:11:22 +0300

    try add A2DP sink

---
+++ LGC-Boombox-firmware/winbox_resources/sources/A2DPSink/Program.cs
@@ -0,0 +1,84 @@
+﻿using CommandLine;
+using Windows.Devices.Enumeration;
+using Windows.Media.Audio;
+
+namespace A2DPSink;
+
+class Program
+{
+    static async Task Main()
+    {
+        //I will change this code a bit as I need it to work automatically.
+        for (;;)
+        {
+            Console.Write("Scanning for devices");
+
+            DeviceInformation? device;
+
+            while ((device = await FindPairedDevice()) is null)
+            {
+                Console.Write(".");
+
+                await Task.Delay(100);
+            }
+
+            Console.WriteLine($"\nDevice {device.Name} found. [Id: {device.Id}]");
+
+            Console.WriteLine($"Connecting to device...");
+
+            try
+            {
+                await ConnectToDevice(device);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to connect: {ex.Message}");
+
+                await Task.Delay(100);
+            }
+        }
+    }
+
+    private static async Task<DeviceInformation?> FindPairedDevice()
+    {
+        var deviceSelector = AudioPlaybackConnection.GetDeviceSelector();
+        var devices = await DeviceInformation.FindAllAsync(deviceSelector);
+
+        return devices.FirstOrDefault();
+    }
+
+    private static async Task ConnectToDevice(DeviceInformation device)
+    {
+        var connection = AudioPlaybackConnection.TryCreateFromId(device.Id) ??
+            throw new InvalidOperationException($"Failed to create AudioPlaybackConnection");
+
+        var promise = new TaskCompletionSource();
+
+        connection.StateChanged += Connection_StateChanged;
+
+        await connection.StartAsync();
+
+        var result = await connection.OpenAsync();
+
+        if (result.Status != AudioPlaybackConnectionOpenResultStatus.Success)
+        {
+            throw new InvalidOperationException($"Failed to open AudioPlaybackConnection: {result.Status}");
+        }
+
+        Console.WriteLine($"Successfully connected.");
+
+        void Connection_StateChanged(AudioPlaybackConnection sender, object args)
+        {
+            if (sender.State == AudioPlaybackConnectionState.Closed)
+            {
+                promise.SetResult();
+
+                connection.StateChanged -= Connection_StateChanged;
+            }
+        }
+
+        await promise.Task;
+    }
+
+}
+
---
+++ LGC-Boombox-firmware/winbox_resources/sources/A2DPSink/README.md
@@ -0,0 +1,12 @@
+# A2DPSink
+
+```
+A2DPSink 1.0.0
+Copyright (C) 2024 A2DPSink
+
+  -d, --device    Required. Name of the bluetooth device to connect to.
+
+  --help          Display this help screen.
+
+  --version       Display version information.
+```
