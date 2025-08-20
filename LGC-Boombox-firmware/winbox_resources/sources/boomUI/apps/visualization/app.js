311ea319c7cab53e2d6c5623fb55ddc5c2aa5481
igorkll <frerther@gmail.com>
2025-08-20 15:45:18 +0300

    fucking web...

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/visualization/app.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/visualization/app.js
@@ -3,5 +3,6 @@ const { ipcRenderer } = require('electron');
 ipcRenderer.on('waves', (event, waves) => {
     for (let i = 0; i <= 5; i++) {
         document.getElementById(`visualization_${i}`).style.height = `${waves[i] * 100}%`;
+        document.getElementById(`visualization_light_${i}`).style.height = `${waves[i] * 100}%`;
     }
 });
