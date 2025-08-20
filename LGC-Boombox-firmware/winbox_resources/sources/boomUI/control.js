2b0b6209c6e1e6a24cb332c47d603922eecfc0b9
igorkll <frerther@gmail.com>
2025-08-21 01:26:01 +0300

    backup

---
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/control.js
@@ -0,0 +1,7 @@
+document.getElementById('poweroff_buffer').addEventListener('custom_click', () => {
+    
+})
+
+document.getElementById('volume_buffer').addEventListener('custom_click', () => {
+    
+})
