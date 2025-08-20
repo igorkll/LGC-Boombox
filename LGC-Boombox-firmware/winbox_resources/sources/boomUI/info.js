aed9f8ecd1e9c09e8c0346195ea02762d8dcac36
igorkll <frerther@gmail.com>
2025-08-21 01:37:04 +0300

    backup

---
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/info.js
@@ -0,0 +1,25 @@
+function updateDateTime() {
+    const now = new Date();
+    
+    const dateOptions = { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' 
+    };
+    const formattedDate = now.toLocaleDateString('en-US', dateOptions);
+    
+    const timeOptions = { 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit', 
+        hour12: false 
+    };
+    const formattedTime = now.toLocaleTimeString('en-US', timeOptions);
+    
+    document.getElementById('showdate').innerText = formattedDate;
+    document.getElementById('showtime').innerText = formattedTime;
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
