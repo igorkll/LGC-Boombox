05afebed91271f5854849861cadc26cfe9dd22db
igorkll <frerther@gmail.com>
2025-08-20 15:33:02 +0300

    Update app.js

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/visualization/app.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/visualization/app.js
@@ -2,6 +2,6 @@ const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('waves', (event, waves) => {
     for (let i = 0; i <= 5; i++) {
-        document.getElementById(`visualization_${i}`).style.height = waves[i] * 100;
+        document.getElementById(`visualization_${i}`).style.height = `${waves[i] * 100}%`;
     }
 });
