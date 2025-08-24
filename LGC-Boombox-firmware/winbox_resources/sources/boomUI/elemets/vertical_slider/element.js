47d133d0e56fb521111b54aaa91d4eb39f6422e1
igorkll <frerther@gmail.com>
2025-08-24 18:23:21 +0300

    fucking custom elements

---
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/vertical_slider/element.js
@@ -0,0 +1,9 @@
+class vertical_slider extends HTMLElement {
+    constructor() {
+        super();
+
+        this.classList.add('soap');
+    }
+}
+
+customElements.define('vertical-slider', vertical_slider);
