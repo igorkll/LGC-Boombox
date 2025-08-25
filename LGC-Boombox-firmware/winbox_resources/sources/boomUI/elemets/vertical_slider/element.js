29fe4273eb79e934991aefc8b8dd05da7df37d8f
igorkll <frerther@gmail.com>
2025-08-25 20:40:50 +0300

    Update element.js

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/vertical_slider/element.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/vertical_slider/element.js
@@ -6,10 +6,14 @@ class vertical_slider extends HTMLElement {
     connectedCallback() {
         let sliderContainer = document.createElement('div');
         sliderContainer.classList.add('soap');
-        sliderContainer.style.setProperty('--slider-color', "#8cd70bff");
+        sliderContainer.style.setProperty('--slider-color', "#8cd70b44");
         sliderContainer.style.flex = 1;
         sliderContainer.style.background = 'var(--slider-color)';
         sliderContainer.style.margin = '2vh';
+
+        let slider = document.createElement('div');
+        slider.style.setProperty('--slider-color', "#8cd70bff");
+        slider.style.background = 'var(--slider-color)';
 
         this.classList.add('soap');
         this.style.display = 'flex';
