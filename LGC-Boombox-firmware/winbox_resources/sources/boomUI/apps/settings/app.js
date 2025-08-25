ce99545562ee77a7f90a9c5a4ed67e4daa7cd09c
igorkll <frerther@gmail.com>
2025-08-25 22:40:52 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/settings/app.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/settings/app.js
@@ -1,9 +1,10 @@
 {
+let tablist = document.getElementById('settings_tablist');
 let tabhost = document.getElementById('settings_tabhost');
 let defaultTab = true;
 
 function addSettingsTab(name) {
-    window.addTab(tabhost, document.getElementById(`setting_${name}_button`), document.getElementById(`setting_${name}_panel`), defaultTab);
+    window.addTab(tablist, tabhost, document.getElementById(`setting_${name}_button`), document.getElementById(`setting_${name}_panel`), defaultTab);
     defaultTab = false;
 }
 
@@ -13,15 +14,15 @@ addSettingsTab("light");
 addSettingsTab("debug");
 
 // wallpaper
-document.getElementById('test1').addEventListener('click', () => {
+document.getElementById('test1').addEventListener('pointerup', () => {
     window.setWallpaper("wallpapers/1.jpg");
 });
 
-document.getElementById('test2').addEventListener('click', () => {
+document.getElementById('test2').addEventListener('pointerup', () => {
     window.setWallpaper("wallpapers/2.jpg");
 });
 
-document.getElementById('test3').addEventListener('click', () => {
+document.getElementById('test3').addEventListener('pointerup', () => {
     window.setWallpaper("wallpapers/3.jpg");
 });
 }
--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/tab/element.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/tab/element.js
@@ -1,12 +1,23 @@
-function addTab(tabhost, button, tab, opened=false) {
+function addTab(tablist, tabhost, button, tab, opened=false) {
     function changeState(state) {
         for (let i = 0; i < tabhost.children.length; i++) {
             const child = tabhost.children[i];
             child.style.display = tab.id === child.id ? 'flex' : 'none';
         }
+
+        for (let i = 0; i < tablist.children.length; i++) {
+            const child = tablist.children[i];
+            if (button.id === child.id) {
+                if (!child.classList.contains('tablink-selected')) {
+                    child.classList.add('tablink-selected');
+                }
+            } else {
+                child.classList.remove('tablink-selected');
+            }
+        }
     }
 
-    button.addEventListener('click', () => {
+    button.addEventListener('pointerup', () => {
         changeState(true);
     });
 
