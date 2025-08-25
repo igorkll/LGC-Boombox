3772db010d7ed91825d46a03f5fc38e5e8f5d5ca
igorkll <frerther@gmail.com>
2025-08-25 20:15:35 +0300

    Update element.js

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/vertical_slider/element.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/elemets/vertical_slider/element.js
@@ -1,10 +1,18 @@
 class vertical_slider extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
     }
 
     connectedCallback() {
+        let sliderContainer = document.createElement('div');
+        sliderContainer.classList.add('soap');
+        sliderContainer.style.setProperty('--slider-color', 'red');
+        sliderContainer.style.width = '100px';
+        sliderContainer.style.height = '50px';
+
         this.classList.add('soap');
+        this.appendChild(sliderContainer);
     }
 }
 
