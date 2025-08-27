83aa2dafc79fa048fedc480f5dcd9885fda1cc92
igorkll <frerther@gmail.com>
2025-08-27 18:55:41 +0300

    Update renderer.js

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/renderer.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/renderer.js
@@ -18,30 +18,34 @@ const { exec } = require('child_process');
 
 // wallpaper
 
-window.setWallpaper = function(path) {
+window.setWallpaper = function(path, dontSave) {
     document.body.style.backgroundImage = `url('${path}')`;
-    localStorage.setItem('wallpaper', path);
+    if (!dontSave) {
+        localStorage.setItem('wallpaper', path);
+    }
 }
 
 window.getWallpaper = function() {
     return localStorage.getItem('wallpaper');
 }
 
-setWallpaper(localStorage.getItem('wallpaper') || "wallpapers/2.jpg");
+setWallpaper(localStorage.getItem('wallpaper') || "wallpapers/2.jpg", true);
 
 // volume
 let max_volume = 65535 * volume_multiplier;
 let current_volume;
 
-window.setVolume = function(volume) {
+window.setVolume = function(volume, dontSave) {
     exec(`nircmd setsysvolume ${Math.round(volume * max_volume)}`);
     current_volume = volume;
-    localStorage.setItem('volume', volume);
+    if (!dontSave) {
+        localStorage.setItem('volume', volume);
+    }
 }
 
 window.getVolume = function() {
     return current_volume;
 }
 
-setVolume(localStorage.getItem('volume') || 0.2);
+setVolume(localStorage.getItem('volume') || 0.2, true);
 }
