64f9efa645c663ea59e6492442f6dd18f69c7d30
igorkll <frerther@gmail.com>
2025-08-27 18:12:41 +0300

    Update main.c

--- led_strip_esp32c3_firmware/main/main.c
+++ led_strip_esp32c3_firmware/main/main.c
@@ -4,21 +4,10 @@
 void app_main() {
     ledstrip_init();
 
+    ledstrip_clear(0x000000);
+    ledstrip_flush();
+
     while (true) {
-        ledstrip_clear(0xff0000);
-        ledstrip_flush();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        ledstrip_clear(0x00ff00);
-        ledstrip_flush();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        ledstrip_clear(0x0000ff);
-        ledstrip_flush();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-
-        ledstrip_clear(0x000000);
-        ledstrip_flush();
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        
     }
 }
