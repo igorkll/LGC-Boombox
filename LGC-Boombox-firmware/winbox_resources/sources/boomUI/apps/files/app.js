4df37b086f0f122b5017c9b6830457affb38b557
igorkll <frerther@gmail.com>
2025-08-30 23:54:48 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/files/app.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/files/app.js
@@ -1,12 +1,14 @@
 {
-let path = require('path');
+const fs = require('fs');
+const path = require('path');
+const drivelist = require('drivelist');
 
-function addFilesFolder(name, path, opened=false) {
-    console.log(path);
-    
-    let tablist = document.getElementById('files_tablist');
-    let tabhost = document.getElementById('files_tabhost');
+let tablist = document.getElementById('files_tablist');
+let tabhost = document.getElementById('files_tabhost');
 
+let tabs = [];
+
+function addFilesFolder(name, path, readonly=false) {
     let tablink = document.createElement('div');
     tablink.id = `${name}_tablink`;
     tablink.classList.add("tablink");
@@ -19,9 +21,27 @@ function addFilesFolder(name, path, opened=false) {
     tab.innerHTML = path;
     tabhost.appendChild(tab);
 
-    addTab(tablist, tabhost, tablink, tab, opened);
+    addTab(tablist, tabhost, tablink, tab);
+    tabs.push({tablink: tablink, tab: tab});
 }
 
 addFilesFolder("defaults", path.join(__dirname, 'defaults'), true);
-addFilesFolder("test2", "D:\\");
+addFilesFolder("storage", 'C:\\LGCBoombox_storage');
+
+activateTab(tablist, tabhost, tabs[0].tablink, tabs[0].tab);
+
+setInterval(1000, async () => {
+    const drives = await drivelist.list();
+
+    console.log(drives);
+
+    for (const drive of drives) {
+        if (drive.mountpoints.some(mp => mp.path.toUpperCase() === 'C:\\')) continue;
+
+        for (const mount of drive.mountpoints) {
+            console.log(mount);
+            addFilesFolder(name, path);
+        }
+    }
+});
 }
