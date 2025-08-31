414bbbe2b69973554874449a02c8fdd17729d007
igorkll <frerther@gmail.com>
2025-08-31 13:06:36 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/music/app.js
+++ LGC-Boombox-firmware/winbox_resources/sources/boomUI/apps/music/app.js
@@ -1,6 +1,8 @@
 {
 let media_player = document.getElementById('media_player');
 let media_panel = document.getElementById('media_panel');
+let music_playPause_img = document.getElementById('music_playPause_img');
+let music_fullscreen_img = document.getElementById('music_fullscreen_img');
 
 let restoreState = null;
 
@@ -13,12 +15,20 @@ window.openVideo = function (path) {
     media_player.play();
 }
 
+function updateGui() {
+    music_fullscreen_img.src = restoreState == null ? 'apps/music/fullscreen.png' : 'apps/music/unfullscreen.png';
+}
+
+updateGui();
+
 music_fullscreen.addEventListener("custom_click", () => {
     if (restoreState != null) {
         restoreState();
         restoreState = null;
+        updateGui();
         return;
     }
     restoreState = fullscreenize(media_panel);
+    updateGui();
 });
 }
