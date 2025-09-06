4a49ccee0664a011c42604e10ddae38487495312
igorkll <frerther@gmail.com>
2025-09-07 00:08:36 +0300

    backup

--- LGC-Boombox-firmware/winbox_resources/sources/BtSwitch/Program.cs
+++ LGC-Boombox-firmware/winbox_resources/sources/BtSwitch/Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Windows.Devices.Radios;
 
 class Program
 {
-    static async Task Main()
+    static async Task Main(string[] args)
     {
         var radios = await Radio.GetRadiosAsync();
-        foreach (var radio in radios)
-        {
-            if (radio.Kind == RadioKind.Bluetooth)
+
+        if (isFlagExists(args, "--enable")) {
+            bool state = getFlagState(args, "--enable");
+
+            foreach (var radio in radios)
             {
-                Console.WriteLine($"Bluetooth найден: {radio.Name}, текущий статус: {radio.State}");
-
-                RadioState newState = radio.State == RadioState.On ? RadioState.Off : RadioState.On;
-                var result = await radio.SetStateAsync(newState);
-
-                // Выводим результат в консоль
-                Console.WriteLine($"Попытка переключить на {newState} вернула: {result}");
+                if (radio.Kind == RadioKind.Bluetooth)
+                {
+                    Console.WriteLine($"bluetooth: {radio.Name}");
+                    Console.WriteLine($"result: {await radio.SetStateAsync(state ? RadioState.On : RadioState.Off)}");
+                }
             }
         }
+    }
+
+    static string? GetArgValue(string[] args, string name)
+    {
+        int index = Array.IndexOf(args, name);
+        if (index >= 0 && index < args.Length - 1)
+            return args[index + 1];
+        return null;
+    }
+
+    static bool getFlagState(string[] args, string name)
+    {
+        return bool.TryParse(GetArgValue(args, name), out bool enableValue);
+    }
+
+    static bool isFlagExists(string[] args, string name)
+    {
+        return Array.Exists(args, arg => arg == name);
     }
 }
