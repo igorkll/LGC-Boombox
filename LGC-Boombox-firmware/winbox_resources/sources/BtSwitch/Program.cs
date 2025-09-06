922028a1a8b3bf33c431bf9526cfa6c65fbf84bc
igorkll <frerther@gmail.com>
2025-09-06 13:47:54 +0300

    backup

---
+++ LGC-Boombox-firmware/winbox_resources/sources/BtSwitch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Radios;
+
+class Program
+{
+    static async Task Main()
+    {
+        var radios = await Radio.GetRadiosAsync();
+        foreach (var radio in radios)
+        {
+            if (radio.Kind == RadioKind.Bluetooth)
+            {
+                Console.WriteLine($"Bluetooth найден: {radio.Name}, статус: {radio.State}");
+
+                if (radio.State == RadioState.Off)
+                {
+                    await radio.SetStateAsync(RadioState.On);
+                    Console.WriteLine("Bluetooth включён.");
+                }
+                else
+                {
+                    await radio.SetStateAsync(RadioState.Off);
+                    Console.WriteLine("Bluetooth выключен.");
+                }
+            }
+        }
+    }
+}
